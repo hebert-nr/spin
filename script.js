dd96af0feaa5b0430e1d5c6ab10fa91278e73063
hebert-nr <nheb461@outlook.com>
2016-04-12 12:17:48 -0700

    Final changes

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 var list = [];
-
-
 //collects the number of users and sets the display based off the number of users entered
 function chooseHouse(){
-
 var val = btn1.value;
 var x = "Please enter the NUMBER of new students."
-
-
 	if(val == 0 || val == null || isNaN(val))
 	{
 		alert(x)
@@ -23,18 +18,15 @@ var x = "Please enter the NUMBER of new students."
 		shuffle();
 	}
 }
-
 /*
 Takes value entered from user and puts into an array, but only counts to 4 then repeats as there are 
 4 houses to be sorted from.
 
 slice removes first index, which is a blank
 */
-
 function loop(){
 	var a = btn1.value;
 	var s = 0;
-	
 	for(var i = 1;i<=a;i++)
 	{
 		if(s >= 4){
@@ -47,8 +39,6 @@ function loop(){
 	}
 	list = list.slice(1);
 }
-
-
 /*
 scrambles the array created above in random order.
 */
@@ -57,7 +47,6 @@ function shuffle(){
 	var i = 0
     , j = 0
     , temp = null;
-
 for (i = list.length - 1; i > 0; i -= 1) {
 		j = Math.floor(Math.random() * (i + 1))
 		temp = list[i];
@@ -66,62 +55,48 @@ for (i = list.length - 1; i > 0; i -= 1) {
 	}
 	document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
-
-
+/* Causes house image to fade in or out. */
 var fadeOut = function(){
         $(".house").fadeOut("slow");
 };
 var fadeIn = function(){
         $(".house").fadeIn(2000);
 };
-
-
-/*
-Pulls the last number from the array and matches it to the house name.
-*/
+/* Pulls the last number from the array and matches it to the house name. */
 function sort(){
 	var house = list.pop();
 	var code = document.getElementsByClassName("house")[0];
 	document.getElementById("wi").style.display="none";
 	code.style.display="none";
 	fadeOut();
-	
 	if (house === 1)
 	{
-		
 		fadeIn();
 		code.style.backgroundImage = 'url("gry.png")';
-		
 	}	
 	else if (house === 2)
 	{
 
 		fadeIn();
 		code.style.backgroundImage = 'url("huf.png")';
-		
 	}
 	else if (house === 3)
 	{
-		
 		fadeIn();
-		code.style.backgroundImage = 'url("rav.png")';
-		
+		code.style.backgroundImage = 'url("rav.png")';	
 	}
 	else if (house ===4)
 	{
-
 		fadeIn();
-		code.style.backgroundImage = 'url("sly.png")';
-		
+		code.style.backgroundImage = 'url("sly.png")';	
 	}
 	else{
+		fadeIn("fast");
 		code.style.backgroundImage='url("hogCrest.png")';
 		document.getElementById("line").innerHTML="Welcome to Hogwarts!!";
 		document.getElementById("sort").style.display = "none";
 	}
-	
 }
-
 //reloads the program so user can start over again 
 function restart(){
 window.location.reload();
